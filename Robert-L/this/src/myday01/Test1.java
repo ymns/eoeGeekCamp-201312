06c0b3caf4944a53ad821f8368d5968f395bd552
Robert-L <1308731627@qq.com>
2013-12-16 20:49:01 +0800

    hello

---
+++ Robert-L/this/src/myday01/Test1.java
@@ -0,0 +1,7 @@
+package myday01;
+
+public class Test1 {
+public static void main(String[] args){
+	System.out.printIn("hello Java!");	
+}
+}
---
+++ Robert-L/this/src/myday01/Test2.java
@@ -0,0 +1,13 @@
+package myday01;
+
+public class Test2 {
+public static void main(String[] args){
+System.out.println("5+3="+(5+3));
+System.out.println("5-3="+(5-3));
+System.out.println("5*3="+5*3);
+
+
+
+
+}
+}
